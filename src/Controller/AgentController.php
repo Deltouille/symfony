5e4a0b60731718a40317085088f3f8d88f7afa07
Deltouille <alexandre.petit7676@gmail.com>
2021-06-19 00:44:08 +0200

    Ajout AgentType
    Réalisation de AgentType + création de la fonction d'ajout d'un agent

--- src/Controller/AgentController.php
+++ src/Controller/AgentController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Agent;
+use App\Form\AgentType;
+
 class AgentController extends AbstractController
 {
     /**
@@ -20,5 +23,24 @@ class AgentController extends AbstractController
         return $this->render('agent/index.html.twig', [
             'listeAgent'=>$listeAgent
         ]);
+    }
+
+    /**
+     * @Route("/agent-ajout", name="agent-ajout")
+     */
+    public function ajout(Request $request): Response
+    {
+        $agent = new Agent();
+        $form = $thhis->createForm(AgentType::class, $agent);
+        if($request->isMethod('POST')){
+            $form->handleRequest($request);
+            if($form->isValid()){
+                $em = $this->getDoctrine()-getManager();
+                $em->persist($agent);
+                $em->flush();
+                return $this->redirectToRoute('agent-ajout');
+            }
+        }
+        return $this->render('agent/ajout.html.twig');
     }
 }
--- src/Form/AgentType.php
+++ src/Form/AgentType.php
@@ -6,18 +6,39 @@ use App\Entity\Agent;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Cible;
+use App\Entity\Planque;
+use App\Entity\Contact;
+use App\Entity\Mission;
 class AgentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('DateNaissance')
-            ->add('codeIdentification')
-            ->add('missions')
-            ->add('Nationnalite')
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('DateNaissance', BirthdayType::class)
+            ->add('codeIdentification', TextType::class)
+            ->add('missions', EntityType::class,
+            array('class' => Mission::class,
+                  'choice_label' => 'titre',
+                  'multiple' => true,
+                  'expanded' => false))
+            ->add('Nationnalite', EntityType::class, array(
+                  'choice_label' => 'nomNation',
+                  'multiple' => true,
+                  'expanded' => false
+                  ))
+            ->add('Sauvegarder', SubmitType::class)
         ;
     }
 
