5a5128837b8076901d2b54b18fb8b7664dbd554c
Deltouille <34238909+Deltouille@users.noreply.github.com>
2021-07-18 23:47:18 +0200

    Update Agent.php

--- src/Entity/Agent.php
+++ src/Entity/Agent.php
@@ -95,6 +95,13 @@ class Agent
     {
         return $this->id;
     }
+    
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+        
+        return $this;
+    }
 
     public function getNom(): ?string
     {
