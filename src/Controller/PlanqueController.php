c30673e6441ac64b829cbe2c9b9dd571e1c9b14e
Deltouille <34238909+Deltouille@users.noreply.github.com>
2021-07-19 14:04:01 +0200

    Update PlanqueController.php

--- src/Controller/PlanqueController.php
+++ src/Controller/PlanqueController.php
@@ -6,7 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use App\Entity\Planque;
 use App\Entity\Pays;
 use App\Form\PlanqueType;
@@ -47,7 +46,7 @@ class PlanqueController extends AbstractController
         $em = $this->getDoctrine()->getManager();
         $planqueRepository = $em->getRepository(Planque::class);
         $suppressionPlanque = $planqueRepository->find($id);
-        if($suppressionPlanque === null){
+        if($suppressionPlanque == null){
             throw new NotFoundHttpException("La planque d'id ".$id." n'existe pas");
         }
         foreach($suppressionPlanque->getMissions() as $mission){
